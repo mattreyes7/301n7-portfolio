74fc9b6174bb7b162a620719d51c749a121748f9
mattreyes7 <mattreyes615@gmail.com>
2017-04-15 10:15:43 -0700

    working on handlebars template

---
+++ starter-code/scripts/projects.js
@@ -0,0 +1,18 @@
+'use strict';
+var sourceHtml = $('#project-template').html();
+var handleBarsTemplate = Handlebars.compile(sourceHtml);
+
+var projects = [
+  {
+    projCategory:'Graphic Design',
+    projTitle:'Federal Way Champs!',
+    projSrc:'assets/fdubchamp.jpg',
+    projSummary:'<p>A t-shirt design made for the 2015 High school basketball state champions.</p>'
+  },
+  {
+    projCategory:'Web Design',
+    projTitle:'Favor',
+    projSrc:'https://mattreyes7.github.io/201-final-project/',
+    projSummary:'<p>My team\'s final project for code fellows 201 class. It is an online \"community board\" web application which maps posts made by users on the google maps API.</p>'
+  },
+]
