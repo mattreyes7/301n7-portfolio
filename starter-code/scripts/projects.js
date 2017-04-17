855c0083fd52b73cd7d79db723410549f459602f
mattreyes7 <mattreyes615@gmail.com>
2017-04-16 19:35:34 -0700

    minor layout fix

--- starter-code/scripts/projects.js
+++ starter-code/scripts/projects.js
@@ -5,13 +5,15 @@ var rawData = {
     {
       projCategory:'Graphic Design',
       projTitle:'Federal Way Champs!',
-      projSrc:'assets/fdubchamp.jpg',
+      projSrc:'assets/fedwaybasketball.jpg',
+      projUrl:'',
       projSummary:'<p>A t-shirt design made for the 2015 High school basketball state champions.</p>'
     },
     {
       projCategory:'Web Design',
       projTitle:'Favor',
-      projSrc:'https://mattreyes7.github.io/201-final-project/',
+      projSrc:'assets/favrlogo.jpg',
+      projUrl:'https://mattreyes7.github.io/201-final-project/',
       projSummary:'<p>My team\'s final project for code fellows 201 class. It is an online \"community board\" web application which maps posts made by users on the google maps API.</p>'
     }
   ]
